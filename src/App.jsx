3cb29d917ef13e9d85b1f2f5928622b9e24e8cf8
Akinwale Alo <aloakinwaleoba@gmail.com>
2026-05-17 14:46:37 +0300

    forgot password link added

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,49 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Landing/Home';
-import Signup from './Guards/Signup';
-import Login from './Guards/Login';
-import Squad from './Landing/Squad';
-import Dashboard from './Components/Dashboard';
-import Gallery from './Landing/Gallery';
-import Event from './Landing/Event';
-import AdminDashboard from './Admin/AdminDashboard';
-import { PlayerProvider } from './context/PlayerContext';
-
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Landing/Home";
+import Signup from "./Guards/Signup";
+import Login from "./Guards/Login";
+import Squad from "./Landing/Squad";
+import Dashboard from "./Components/Dashboard";
+import Gallery from "./Landing/Gallery";
+import Event from "./Landing/Event";
+import AdminDashboard from "./Admin/AdminDashboard";
+import { PlayerProvider } from "./context/PlayerContext";
+import ForgotPassword from "./Guards/ForgotPassword";
 
 function App() {
   const isLoggedIn = !!localStorage.getItem("player");
-  
+
   return (
-  
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
-        <Route path="/signup" element={<Signup/>} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/squad" element={<Squad />} />
         <Route path="/gallery" element={<Gallery />} />
-        <Route path="/dashboard" element={
-          <PlayerProvider>
-            <Dashboard />
-          </PlayerProvider>
-        } />
+        <Route
+          path="/dashboard"
+          element={
+            <PlayerProvider>
+              <Dashboard />
+            </PlayerProvider>
+          }
+        />
         <Route path="/Event" element={<Event />} />
-        <Route path="/admin" element={
-          <PlayerProvider>
-            <AdminDashboard />
-          </PlayerProvider>
-        } />
+        <Route
+          path="/admin"
+          element={
+            <PlayerProvider>
+              <AdminDashboard />
+            </PlayerProvider>
+          }
+        />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
